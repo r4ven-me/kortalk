33f3ba8b9e97c651d113a66a6db59ae79b98e3ec
Ivan Cherniy <kar-kar@r4ven.me>
2026-07-23 17:29:59 +0300

    Upd

--- src/kortalk/theme.py
+++ src/kortalk/theme.py
@@ -263,7 +263,7 @@ def window_stylesheet(colors: dict[str, str]) -> str:
             width: 4px; margin: 6px 0px; border-radius: 2px;
         }}
         QSplitter::handle:vertical {{
-            height: 7px; border-radius: 2px;
+            height: 9px; margin: 2px 0px; border-radius: 2px;
         }}
         QSplitter::handle:hover {{ background-color: {c['highlight']}; }}
 
